00870a83fedd6680d6c6ca5baf522a14efc404ca
Desmondonam <desmondonam@gmail.com>
2023-07-12 22:52:10 +0300

    add html

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html', title='Home')
+
+
+@app.route('/about')
+def about():
+    return render_template('index.html', title='About')
+
 
 if __name__ == '__main__':
     app.run()
