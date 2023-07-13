bc68e09d156d37a5729fdb3053bb1d7f3a78cf27
Desmondonam <desmondonam@gmail.com>
2023-07-13 22:49:51 +0300

    final basics

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('Name')
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return render_template('index.html', title='Home')
-
-
-@app.route('/about')
-def about():
-    return render_template('index.html', title='About')
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        return render_template('greeting.html', name=name)
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
